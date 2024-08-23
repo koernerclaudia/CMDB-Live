45b2ea0f91ab456bdb8ffdc810747f63a3c54c66
Claudia Koerner <claudia.koerner@gmail.com>
2024-08-23 15:10:43 +0200

    Built NavigationBar and ProfilePage
    - setup the navigation bar with routes for loggedin / logged out users
    - set up a profile page listing favourite movies
    
    missing + problems:
    - when logging in - the profile only shows favourited movies or the movie list when refreshing the page
    - the profile page does not yet have a form to make changes to the user's profile

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies }) => {
+  const { MovieID } = useParams();
+  const movie = movies.find((m) => m._id === MovieID);
+
   return (
     <>
-     
-    <Card style={{ width: '40rem' }}  >
+    <Card style={{ width: '40rem' }} border="info" >
     <Card.Img variant="top" src={movie.ImageURL} className="img-in-view"/>
     <Card.Body>
     <Card.Title>{movie.Title}</Card.Title>
@@ -21,8 +24,10 @@ export const MovieView = ({ movie, onBackClick }) => {
     <ListGroup.Item>Director: {movie.Director.Name}</ListGroup.Item>
     <ListGroup.Item>Genre: {movie.Genre.Type} ( {movie.Genre.Description})</ListGroup.Item>
     </ListGroup>
-    </Card>
-    <Button className="margin-top" onClick={onBackClick}>Back to List of Movies</Button>
+    </Card><br />
+    <Link to={`/`}>
+        <Button variant="info">Back to list of Movies</Button>
+      </Link>
     </>
 
 );}
